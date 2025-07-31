6c420aa9b429942d289c701bef56ed6b3de7e782
niranjan-uma-shankar <niranjan.u@gmail.com>
2025-07-31 10:11:18 +0530

    WIP: Changes to detect alias timezone names

---
+++ packages/timezone-data/src/data.ts
@@ -0,0 +1,319 @@
+export const timezoneData: {name: string; label: string}[] = [
+    {
+        name: 'Pacific/Pago_Pago',
+        label: 'Midway Island, Samoa'
+    },
+    {
+        name: 'Pacific/Honolulu',
+        label: 'Hawaii'
+    },
+    {
+        name: 'America/Anchorage',
+        label: 'Alaska'
+    },
+    {
+        name: 'America/Tijuana',
+        label: 'Chihuahua, La Paz, Mazatlan'
+    },
+    {
+        name: 'America/Los_Angeles',
+        label: 'Pacific Time (US & Canada); Tijuana'
+    },
+    {
+        name: 'America/Phoenix',
+        label: 'Arizona'
+    },
+    {
+        name: 'America/Denver',
+        label: 'Mountain Time (US & Canada)'
+    },
+    {
+        name: 'America/Costa_Rica',
+        label: 'Central America'
+    },
+    {
+        name: 'America/Chicago',
+        label: 'Central Time (US & Canada)'
+    },
+    {
+        name: 'America/Mexico_City',
+        label: 'Guadalajara, Mexico City, Monterrey'
+    },
+    {
+        name: 'America/Regina',
+        label: 'Saskatchewan'
+    },
+    {
+        name: 'America/Bogota',
+        label: 'Bogota, Lima, Quito'
+    },
+    {
+        name: 'America/New_York',
+        label: 'Eastern Time (US & Canada)'
+    },
+    {
+        name: 'America/Fort_Wayne',
+        label: 'Indiana (East)'
+    },
+    {
+        name: 'America/Caracas',
+        label: 'Caracas, La Paz'
+    },
+    {
+        name: 'America/Halifax',
+        label: 'Atlantic Time (Canada); Greenland'
+    },
+    {
+        name: 'America/Santiago',
+        label: 'Santiago'
+    },
+    {
+        name: 'America/St_Johns',
+        label: 'Newfoundland'
+    },
+    {
+        name: 'America/Argentina/Buenos_Aires',
+        label: 'Buenos Aires, Brasilia, Georgetown'
+    },
+    {
+        name: 'America/Noronha',
+        label: 'Fernando de Noronha'
+    },
+    {
+        name: 'Atlantic/Azores',
+        label: 'Azores'
+    },
+    {
+        name: 'Atlantic/Cape_Verde',
+        label: 'Cape Verde Is.'
+    },
+    {
+        name: 'Etc/UTC',
+        label: 'UTC'
+    },
+    {
+        name: 'Africa/Casablanca',
+        label: 'Casablanca, Monrovia'
+    },
+    {
+        name: 'Europe/Dublin',
+        label: 'Dublin, Edinburgh, London'
+    },
+    {
+        name: 'Europe/Amsterdam',
+        label: ' Amsterdam, Berlin, Rome, Stockholm, Vienna'
+    },
+    {
+        name: 'Europe/Prague',
+        label: 'Belgrade, Bratislava, Budapest, Prague'
+    },
+    {
+        name: 'Europe/Paris',
+        label: 'Brussels, Copenhagen, Madrid, Paris'
+    },
+    {
+        name: 'Europe/Warsaw',
+        label: 'Sarajevo, Skopje, Warsaw, Zagreb'
+    },
+    {
+        name: 'Africa/Lagos',
+        label: 'West Central Africa'
+    },
+    {
+        name: 'Europe/Athens',
+        label: 'Athens, Beirut, Bucharest'
+    },
+    {
+        name: 'Africa/Cairo',
+        label: 'Cairo, Egypt'
+    },
+    {
+        name: 'Africa/Maputo',
+        label: 'Harare'
+    },
+    {
+        name: 'Europe/Kiev', // Changing name to 'Europe/Kiev', and keeping the UI with Kyiv. Change this once we are passed the moment lib update.
+        label: 'Helsinki, Kyiv, Riga, Sofia, Tallinn, Vilnius'
+    },
+    {
+        name: 'Asia/Jerusalem',
+        label: 'Jerusalem'
+    },
+    {
+        name: 'Africa/Johannesburg',
+        label: 'Pretoria'
+    },
+    {
+        name: 'Asia/Baghdad',
+        label: 'Baghdad'
+    },
+    {
+        name: 'Asia/Riyadh',
+        label: 'Kuwait, Nairobi, Riyadh'
+    },
+    {
+        name: 'Europe/Istanbul',
+        label: 'Istanbul, Ankara'
+    },
+    {
+        name: 'Europe/Moscow',
+        label: 'Moscow, St. Petersburg, Volgograd'
+    },
+    {
+        name: 'Asia/Tehran',
+        label: 'Tehran'
+    },
+    {
+        name: 'Asia/Dubai',
+        label: 'Abu Dhabi, Muscat'
+    },
+    {
+        name: 'Asia/Baku',
+        label: 'Baku, Tbilisi, Yerevan'
+    },
+    {
+        name: 'Asia/Kabul',
+        label: 'Kabul'
+    },
+    {
+        name: 'Asia/Karachi',
+        label: 'Islamabad, Karachi, Tashkent'
+    },
+    {
+        name: 'Asia/Yekaterinburg',
+        label: 'Yekaterinburg'
+    },
+    {
+        name: 'Asia/Kolkata',
+        label: 'Chennai, Kolkata, Mumbai, New Delhi'
+    },
+    {
+        name: 'Asia/Kathmandu',
+        label: 'Katmandu'
+    },
+    {
+        name: 'Asia/Almaty',
+        label: 'Almaty, Novosibirsk'
+    },
+    {
+        name: 'Asia/Dhaka',
+        label: 'Astana, Dhaka, Sri Jayawardenepura'
+    },
+    {
+        name: 'Asia/Rangoon',
+        label: 'Rangoon'
+    },
+    {
+        name: 'Asia/Bangkok',
+        label: 'Bangkok, Hanoi, Jakarta'
+    },
+    {
+        name: 'Asia/Krasnoyarsk',
+        label: 'Krasnoyarsk'
+    },
+    {
+        name: 'Asia/Hong_Kong',
+        label: 'Beijing, Chongqing, Hong Kong, Urumqi'
+    },
+    {
+        name: 'Asia/Irkutsk',
+        label: 'Irkutsk, Ulaan Bataar'
+    },
+    {
+        name: 'Asia/Singapore',
+        label: 'Kuala Lumpur, Perth, Singapore, Taipei'
+    },
+    {
+        name: 'Asia/Tokyo',
+        label: 'Osaka, Sapporo, Tokyo'
+    },
+    {
+        name: 'Asia/Seoul',
+        label: 'Seoul'
+    },
+    {
+        name: 'Asia/Yakutsk',
+        label: 'Yakutsk'
+    },
+    {
+        name: 'Australia/Adelaide',
+        label: 'Adelaide'
+    },
+    {
+        name: 'Australia/Darwin',
+        label: 'Darwin'
+    },
+    {
+        name: 'Australia/Brisbane',
+        label: 'Brisbane, Guam, Port Moresby'
+    },
+    {
+        name: 'Australia/Sydney',
+        label: 'Canberra, Hobart, Melbourne, Sydney, Vladivostok'
+    },
+    {
+        name: 'Asia/Magadan',
+        label: 'Magadan, Soloman Is., New Caledonia'
+    },
+    {
+        name: 'Pacific/Auckland',
+        label: 'Auckland, Wellington'
+    },
+    {
+        name: 'Pacific/Fiji',
+        label: 'Fiji, Kamchatka, Marshall Is.'
+    },
+    {
+        name: 'Pacific/Kwajalein',
+        label: 'International Date Line West'
+    }
+];
+
+export const aliasTimezones = {
+    'Pacific/Samoa': 'Pacific/Pago_Pago',
+    'Pacific/Midway': 'Pacific/Pago_Pago',
+    'Pacific/Johnston': 'Pacific/Honolulu',
+    'America/Tijuana': 'America/Los_Angeles',
+    'America/Creston': 'America/Phoenix',
+    'America/Shiprock': 'America/Denver',
+    'America/Indiana/Indianapolis': 'America/Fort_Wayne',
+    'America/Buenos_Aires': 'America/Argentina/Buenos_Aires',
+    'Europe/Brussels': 'Europe/Amsterdam',
+    'Europe/Bratislava': 'Europe/Prague',
+    'Europe/Monaco': 'Europe/Paris',
+    'Africa/Bangui': 'Africa/Lagos',
+    'Africa/Brazzaville': 'Africa/Lagos',
+    'Africa/Douala': 'Africa/Lagos',
+    'Africa/Kinshasa': 'Africa/Lagos',
+    'Africa/Libreville': 'Africa/Lagos',
+    'Africa/Luanda': 'Africa/Lagos',
+    'Africa/Malabo': 'Africa/Lagos',
+    'Africa/Niamey': 'Africa/Lagos',
+    'Africa/Porto-Novo': 'Africa/Lagos',
+    'Africa/Blantyre': 'Africa/Maputo',
+    'Africa/Bujumbura': 'Africa/Maputo',
+    'Africa/Gaborone': 'Africa/Maputo',
+    'Africa/Harare': 'Africa/Maputo',
+    'Africa/Kigali': 'Africa/Maputo',
+    'Africa/Lubumbashi': 'Africa/Maputo',
+    'Africa/Lusaka': 'Africa/Maputo',
+    'Asia/Tel_Aviv': 'Asia/Jerusalem',
+    'Africa/Maseru': 'Africa/Johannesburg',
+    'Africa/Mbabane': 'Africa/Johannesburg',
+    'Asia/Aden': 'Asia/Riyadh',
+    'Asia/Kuwait': 'Asia/Riyadh',
+    'Asia/Istanbul': 'Europe/Istanbul',
+    'Indian/Reunion': 'Asia/Dubai',
+    'Indian/Mahe': 'Asia/Dubai',
+    'Asia/Muscat': 'Asia/Dubai',
+    'Asia/Calcutta': 'Asia/Kolkata',
+    'Asia/Katmandu': 'Asia/Kathmandu',
+    'Asia/Dacca': 'Asia/Dhaka',
+    'Asia/Yangon': 'Asia/Rangoon',
+    'Asia/Phnom_Penh': 'Asia/Bangkok',
+    'Asia/Vientiane': 'Asia/Bangkok',
+    'Indian/Christmas': 'Asia/Bangkok',
+    'Asia/Kuala_Lumpur': 'Asia/Singapore',
+    'Australia/Canberra': 'Australia/Sydney',
+} as const;
+
